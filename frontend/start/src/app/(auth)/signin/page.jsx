253efe8ece92cb589c1f93ecbb44afb5463a7f3f
mohammadmahdijerban <mohammadmahdijerban@gmail.com>
2025-09-09 14:10:31 +0330

    added context

---
+++ frontend/start/src/app/(auth)/signin/page.jsx
@@ -0,0 +1,80 @@
+"use client";
+import Button from "@/ui/Button";
+import RHFTextField from "@/ui/RHFTextField";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import Link from "next/link";
+import SpinnerMini from "@/ui/SpinnerMini";
+import { useAuth } from "@/context/AuthContext";
+
+const schema = yup
+  .object({
+    email: yup.string().email("ایمیل نامعتبر است").required("ایمیل الزامی است"),
+    password: yup.string().required("رمز عبور الزامی است"),
+  })
+  .required();
+
+function Signin() {
+  const { signin } = useAuth();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm({
+    resolver: yupResolver(schema),
+    mode: "onTouched",
+  });
+
+  const onSubmit = async (values) => {
+    await signin(values);
+  };
+
+  return (
+    <div className="max-w-md mx-auto mt-12 px-6 py-8 bg-white shadow-md rounded-2xl">
+      <h1 className="text-2xl font-bold text-secondary-600 text-center mb-8">
+        ورود
+      </h1>
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
+        <RHFTextField
+          label="ایمیل"
+          name="email"
+          register={register}
+          dir="ltr"
+          isRequired
+          errors={errors}
+        />
+        <RHFTextField
+          label="رمز عبور"
+          name="password"
+          register={register}
+          type="password"
+          dir="ltr"
+          isRequired
+          errors={errors}
+        />
+        <div>
+          {isSubmitting ? (
+            <SpinnerMini />
+          ) : (
+            <Button type="submit" variant="primary" className="w-full">
+              تایید
+            </Button>
+          )}
+        </div>
+      </form>
+      <p className="mt-6 text-sm flex justify-center gap-1">
+        حساب نساختید؟{" "}
+        <Link
+          href="/signup"
+          className="text-secondary-500 font-semibold hover:text-blue-600"
+        >
+          ثبت نام
+        </Link>
+      </p>
+    </div>
+  );
+}
+
+export default Signin;
---
+++ frontend/start/src/app/layout.jsx
@@ -0,0 +1,25 @@
+import Navbar from "@/components/NavBar";
+import Footer from "@/components/Footer";
+import vazirFont from "@/constants/localFont";
+import "@/styles/globals.css";
+import { Toaster } from "react-hot-toast";
+import AuthProvider from "@/context/AuthContext";
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="fa" dir="rtl">
+      <body className={`${vazirFont.variable} font-sans`}>
+        <AuthProvider>
+          <div className="flex flex-col min-h-screen">
+            <Toaster />
+
+            <main className="flex-grow container mx-auto px-4 py-6">
+              {children}
+            </main>
+
+          </div>
+        </AuthProvider>
+      </body>
+    </html>
+  );
+}
